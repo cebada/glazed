105570cce75507b586087452d7a5d243b2453a26
João Reis <reisjoao97@hotmail.com>
2021-05-07 23:59:35 +0100

    Prepared config for heroku

--- config/index.js
+++ config/index.js
@@ -1,13 +1,9 @@
 require('dotenv').config();
 
 module.exports = {
-    APPLICATION_PORT: process.env.APPLICATION_PORT || 3000,
-    //MONGODB_URL: process.env.MONGODB_URL,
-    MONGODB_URL: "mongodb+srv://testuser:test123@glazed-cluster.eahyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    //SECRET_KEY: process.env.SECRET_KEY,
-    SECRET_KEY: "reshdjfjgkhjbkvtjcr",
-    //START_DELIVERY_TIME: process.env.START_DELIVERY_TIME,
-    START_DELIVERY_TIME: "08:00",
-    //END_DELIVERY_TIME: process.env.END_DELIVERY_TIME
-    END_DELIVERY_TIME: "22:00"
+    APPLICATION_PORT: process.env.APPLICATION_PORT,
+    MONGODB_URL: process.env.MONGODB_URL,
+    SECRET_KEY: process.env.SECRET_KEY,
+    START_DELIVERY_TIME: process.env.START_DELIVERY_TIME,
+    END_DELIVERY_TIME: process.env.END_DELIVERY_TIME
 }
